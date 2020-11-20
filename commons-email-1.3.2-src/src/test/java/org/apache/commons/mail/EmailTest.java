251197aeb3ea45f7d81c477882b0149b0d50c963
Mariam <mbit@umich.edu>
2020-11-19 21:12:39 -0500

    Added test for Email addBcc(String... emails): testAddBcc()

--- commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
+++ commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -1,10 +1,15 @@
 package org.apache.commons.mail;
 
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class EmailTest {
 	
+	private static String[] emailList = {"email1@gmail.com", "email2@gmail.com", "email3@gmail.com", "email4@gmail.com", "email5@gmail.com"};
+	private static String[] emptyEmailList = {};
 	private EmailConcrete email;
 	
 	@Before
@@ -18,4 +23,13 @@ public class EmailTest {
 	{
 
 	}
+	
+	@Test
+	public void testAddBcc() throws Exception
+	{
+		email.addBcc(emailList);
+		assertNotNull(email.getBccAddresses());
+		assertEquals(5, email.getBccAddresses().size());	
+	}
+	
 }
