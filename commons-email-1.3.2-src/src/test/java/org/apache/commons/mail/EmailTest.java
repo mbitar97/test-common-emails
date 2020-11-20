3238338d3bed3bdd4f66f1543881d444dce6600b
Mariam <mbit@umich.edu>
2020-11-19 20:58:27 -0500

    Added the setup method: setUpEmailTest()

--- commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
+++ commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -1,7 +1,15 @@
 package org.apache.commons.mail;
 
+import org.junit.Before;
+
 public class EmailTest {
 	
 	private EmailConcrete email;
-
+	
+	@Before
+	public void setUpEmailTest() throws Exception
+	{
+		email = new EmailConcrete();
+	}
+	
 }
